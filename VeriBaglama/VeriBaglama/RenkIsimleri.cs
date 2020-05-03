deaebbeb125b1a8d0efa9c95dbcd53ec28bb2283
celalaksu <32665644+celalaksu@users.noreply.github.com>
2020-05-03 23:15:55 +0300

    Proje dosyası ekle.

---
+++ VeriBaglama/VeriBaglama/RenkIsimleri.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace VeriBaglama
+{
+    public class RenkIsimleri
+    {
+        public RenkIsimleri()
+        {
+
+        }
+
+        public string RenkAdi { private set; get; }
+
+        public string BilinenIsim { private set; get; }
+
+        public Color Renk { private set; get; }
+
+        // Static members.
+        static RenkIsimleri()
+        {
+            List<RenkIsimleri> butunRenkler = new List<RenkIsimleri>();
+            StringBuilder metinTasarlayici = new StringBuilder();
+
+            // Loop through the public static fields of the Color structure.
+            foreach (FieldInfo alanBilgisi in typeof(Color).GetRuntimeFields())
+            {
+                if (alanBilgisi.IsPublic &&
+                    alanBilgisi.IsStatic &&
+                    alanBilgisi.FieldType == typeof(Color))
+                {
+                    // Convert the name to a friendly name.
+                    string name = alanBilgisi.Name;
+                    metinTasarlayici.Clear();
+                    int index = 0;
+
+                    foreach (char ch in name)
+                    {
+                        if (index != 0 && Char.IsUpper(ch))
+                        {
+                            metinTasarlayici.Append(' ');
+                        }
+                        metinTasarlayici.Append(ch);
+                        index++;
+                    }
+
+                    // Instantiate a NamedColor object.
+                    RenkIsimleri renkIsimleri = new RenkIsimleri
+                    {
+                        RenkAdi = name,
+                        BilinenIsim = metinTasarlayici.ToString(),
+                        Renk = (Color)alanBilgisi.GetValue(null)
+                    };
+
+                    // Add it to the collection.
+                    butunRenkler.Add(renkIsimleri);
+                }
+            }
+            butunRenkler.TrimExcess();
+            ButunRenkler = butunRenkler;
+        }
+
+        public static IEnumerable<RenkIsimleri> ButunRenkler { private set; get; }
+    }
+}
---
+++ VeriBaglama/VeriBaglama/VeriBaglamaModlar.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VeriBaglama
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VeriBaglamaModlar : ContentPage
+    {
+        public VeriBaglamaModlar()
+        {
+            InitializeComponent();
+        }
+    }
+}
